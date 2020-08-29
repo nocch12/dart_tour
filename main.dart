7c0a4cd6e80d16e5d9c2c7ea2000be4d9fe0c515
nocch12 <k.ohno12.gt@gmail.com>
2020-08-29 21:46:37 +0900

    first

---
+++ main.dart
@@ -0,0 +1,37 @@
+printInteger(int aNumber) {
+  print('The number is $aNumber.');
+}
+
+main() {
+  // var number = 42;
+  // printInteger(number);
+
+  // final name = 'Bob';
+  // final String nickname = 'Bobby';
+  // name = 'Alice'; error
+
+  // const bar = 10000;
+  // const double atm = 1.01325 * bar;
+
+  // var foo = [];
+  // var bar = const [];
+  // const baz = [];
+
+  // foo = [1, 2, 3];
+  // bar = [1, 2];
+  // baz = [42]; error
+
+  // const Object i = 3;
+  // const list = [i as int]; // [3]
+  // const map = {if (i is int) i: "int"}; // {3: int}
+  // const set = {if (list is List<int>) ...list}; // {3}
+
+  // var x = 1; // int
+  // var hex = 0xDEADBEEF; // int
+
+  // var y = 1.1; // double
+  // var exponents = 1.42e5; // double
+  // double z = 1; // 1.0
+
+  // print(z);
+}
